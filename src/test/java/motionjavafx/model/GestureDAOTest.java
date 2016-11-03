da39f1a44ea92ce64b3e6f53f6804e43480fae02
Souse <chrioswald@gmail.com>
2016-11-03 13:25:07 +0100

    add test stub

---
+++ src/test/java/motionjavafx/model/GestureDAOTest.java
@@ -0,0 +1,27 @@
+package motionjavafx.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lena on 11/3/16.
+ */
+public class GestureDAOTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllGestures() throws Exception {
+
+    }
+
+    @Test
+    public void testInsertGesture() throws Exception {
+
+    }
+}
